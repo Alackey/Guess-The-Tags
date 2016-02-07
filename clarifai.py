c0d64f612697190f9ee64ef13f1f49aa5f031a02
Jonathan <gameguy94@hotmail.com>
2016-02-06 17:54:41 -0800

    update clarifai

--- clarifai.py
+++ clarifai.py
@@ -6,5 +6,20 @@ clarifai_api = ClarifaiApi()  # assumes environment variables are set.
 URLArray = imgur.getImages()
 imageURL = URLArray[0]
 result = clarifai_api.tag_image_urls(imageURL)
-pprint.pprint(result)
+
+
+
+def writeToFile(result):
+    with open('data.txt', 'w') as outfile:
+        json.dump(result, outfile)
+    outfile.close()
+    
+def getTags(result):
+    
+    tags = result["results"][0]["result"]["tag"]["classes"]
+    
+    return tags
+#pprint.pprint(result)
+#pprint.pprint(result["results"][0]["result"]["tag"]["classes"])
+#getTags(result)
 
