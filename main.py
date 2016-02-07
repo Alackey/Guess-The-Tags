b2e2ec9359323b15fe83657bba28a6f6b9b4659b
Jonathan <gameguy94@hotmail.com>
2016-02-06 23:15:01 -0800

    messing around

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
+import imgur
+import clarafai
 @app.route('/')
 def hello_world():
-    return render_template('base.html')
+    image = randImage()
+    return render_template('base.html', image=image )
+    
+def randImage():
+    URLArray = imgur.getImages()
+    imageURL = URLArray[0]
+    return imageURL
 
 if __name__ == '__main__':
     app.run()
