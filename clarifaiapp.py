b52681e5b54c7aa7d019022cb4d290d824a08cce
Anthony Lackey <alackey96@gmail.com>
2016-02-07 10:50:18 -0800

    Won alert done

--- clarifaiapp.py
+++ clarifaiapp.py
@@ -13,10 +13,12 @@ def getContent():
     result2 = clarifai_api.tag_image_urls(URLArray[1]['url'])
     print('for loop')
     tags = []
+    count = 0
     for i in range(0, 12):
         rand = randint(0, 1)
         if rand == 0:
             tags.append(result1["results"][0]["result"]["tag"]["classes"][i])
+            count += 1
         else:
             tags.append(result2["results"][0]["result"]["tag"]["classes"][i])
 
@@ -25,7 +27,8 @@ def getContent():
         'tags': result1["results"][0]["result"]["tag"]["classes"],
         'choices': tags,
         'width': URLArray[0]['width'],
-        'height': URLArray[0]['height']
+        'height': URLArray[0]['height'],
+        'numOfTags': count
     }
 
     return imgDetails
