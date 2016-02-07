fa77f64c47d36ffd9c149720c5bb91d3502dd44f
Anthony Lackey <alackey96@gmail.com>
2016-02-06 18:29:54 -0800

    Init database code

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class score(db.Model):
+    rank = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False)
+    score = db.Column(db.Integer, unique=False)
+
+    def __init__(self, rank, username, score):
+        self.rank = rank
+        self.username = username
+        self.score = score
+
 
 @app.route('/')
-def hello_world():
+def homepage():
     return render_template('base.html')
+
+
+@app.route('/highscore')
+def highscores():
+    print("true/false")
+    print(request.form)
+
 
 if __name__ == '__main__':
     app.run()
